adf713a32efece5f8281bafc8b5e33c957a7b140
Jiwoo Jeong <realrv0801@gmail.com>
2026-09-19 16:40:26 -0700

    minor fix

--- components/jangoing-cross-lingual.tsx
+++ components/jangoing-cross-lingual.tsx
@@ -71,9 +71,6 @@ export function JangoingCrossLingual() {
       </p>
 
       <div className="jg-xling-contract">
-        <p className="jg-xling-deployment-status">
-          PLANNED EVALUATION · NOT YET DEPLOYED
-        </p>
         <div
           className="jg-xling-tabs"
           role="tablist"
